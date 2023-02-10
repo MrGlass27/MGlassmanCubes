e48d6f7b7143f3df6c4a31ed741b92d9fc4d3613
matthew <m1glassman@student.bridgew.edu>
2023-02-09 20:42:55 -0500

    finished tests and changed api key name.

--- Tests/tests.py
+++ Tests/tests.py
@@ -1,19 +1,34 @@
 from form_collection import get_json
+from db_setup import push_to_table
 import sqlite3
 
 
 def test_form_number():
     json_response = get_json()
     json_response = json_response['Entries']
-    assert json_response >= 10
+    assert len(json_response) >= 10
 
 
-
- def test_database_exists():
-     name = 'cubes_database.db'
-     connection = sqlite3.connect(name)
-     cursor = connection.cursor()
-     results = cursor.fetchall()
-     cursor.close()
-     connection.close()
-     assert results != []
+def test_database_exists():
+    json_response = get_json()
+    json_response = json_response['Entries']
+    name = 'cubes_database.db'
+    connection = sqlite3.connect(name)
+    cursor = connection.cursor()
+    cursor.execute('''CREATE TABLE IF NOT EXISTS cubes_table (
+                                       id INTEGER PRIMARY KEY,
+                                       firstname text NOT NULL,
+                                       lastname text NOT NULL,
+                                       title text,
+                                       organization text NOT NULL,
+                                       email text NOT NULL,
+                                       website text,
+                                       phone text,
+                                       opportunities text,
+                                       time text,
+                                       permission text
+                                   ); ''')
+    cursor.execute('''DELETE FROM cubes_table''')
+    push_to_table(json_response, cursor)
+    results = cursor.execute("SELECT * from cubes_table").fetchall()
+    assert results != []
